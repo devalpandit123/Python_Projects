9730d085068c9518d4ba405be95c950cecf764e6
Deval Pandit <deval.pandit@gm.com>
2021-12-24 18:30:04 +0000

    Added instructions in code

--- Color_Choice_Game.py
+++ Color_Choice_Game.py
@@ -1,26 +1,43 @@
-import random
+# *******************Project 1: Color Choice Game******************************
 
-print("Winning Rules of the Colors choices Game as follows: "+ "\nEnter a number from one two five and match computer choice to Win the computer.")
+# Project Helpful Notes:
+# 1.We need to create choices for players and choices for computer then ask the players for inputs.
+# 2.Compare their inputs with the choice of the computer.
+# 3.Check if the game is won, tied, lost, or ongoing.
+# 4.Repeat thess steps until the game has been won or tied.
+# 5.Ask if players want to play again.
+# 6.If the players agree restart the game again.
+# 7.If the player say "No" end the game and say thanks for the player.
+
+# *****************************************************************************
+
+import random
+print("Winning Rules of the Colors choices Game as follows: " +
+      "\nEnter a number from one two five and match computer choice to Win the computer.")
 print("red = 1 \nyellow = 2 \norange = 3 \ngreen = 4 \nblue = 5 \ntake a turn: ")
 user, computer, counter = 0, 0, 0
 playButton = input("Do you want to play?(Y/N) ")
-while(True):  
+while(True):
     if(playButton == 'Y' or playButton == 'y'):
-        numColorDict = {1: "red", 2: "yellow", 3: "orange", 4: "green", 5: "blue"}
+        numColorDict = {1: "red", 2: "yellow",
+                        3: "orange", 4: "green", 5: "blue"}
         while counter != 1:
-            userSelection = input("Select a number between 1 and 5, press N/n to end the game: ")
+            userSelection = input(
+                "Select a number between 1 and 5, press N/n to end the game: ")
             print("-----------{}".format(numColorDict.keys()))
             try:
                 if int(userSelection) in numColorDict.keys():
-                    print("Your selected color : {}. Let us see what computer selects the color".format(numColorDict.get(int(userSelection))))
-                    computerSelection = random.randint(1,5)
-                    print("Computer selected : {}".format(numColorDict.get(int(computerSelection))))
+                    print("Your selected color : {}. Let us see what computer selects the color".format(
+                        numColorDict.get(int(userSelection))))
+                    computerSelection = random.randint(1, 5)
+                    print("Computer selected : {}".format(
+                        numColorDict.get(int(computerSelection))))
                     if int(userSelection) == computerSelection:
                         user += 1
                     else:
                         computer += 1
-                    print("User points : ",user)
-                    print("Computer points : ",computer)
+                    print("User points : ", user)
+                    print("Computer points : ", computer)
                 else:
                     print("Invalid input, please try again!")
                 break
